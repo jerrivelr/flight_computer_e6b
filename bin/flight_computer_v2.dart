878c9331280fab5d806c81145169de32a3ef0892
Jerrivel Rodriguez <Jerrivelr@gmail.com>
2023-08-29 16:27:58 -0400

    Input checker function deleted since is no longer need after creating the optionMenu function in simple_io.dart

--- bin/flight_computer_v2.dart
+++ bin/flight_computer_v2.dart
@@ -1,6 +1,7 @@
 import 'package:dart_console/dart_console.dart';
 import 'package:flight_e6b/menu_screens.dart';
 import 'package:flight_e6b/simple_io.dart';
+import 'package:flight_e6b/menu_logic.dart';
 
 final console = Console();
 
@@ -10,8 +11,7 @@ void main(List<String> arguments) {
   while (userSelection != 'exit') {
     switch (userSelection) {
       case 'menu':
-        mainMenu();
-        userSelection = optionChecker();
+        userSelection = mainMenu();
         break;
       case 'opt1':
         console.clearScreen();
@@ -40,55 +40,4 @@ void main(List<String> arguments) {
 
     }
   }
-}
-
-String? optionChecker() {
-  console.writeLine();
-  console.setForegroundExtendedColor(250);
-  String userInput = input(': ') ?? '';
-
-  if (userInput == 'exit') {
-    return userInput;
-  }
-
-  int? option = int.tryParse(userInput);
-  // If more options are added, remember to increase the condition in the while statement (option > x)
-  // to check for out of range selection
-  while (option == null || option < 1 || option > 7) {
-    if (option == null) {
-      console.clearScreen();
-      mainMenu();
-      errorMessage('Enter a valid option');
-
-      option = int.tryParse(input(': ') ?? '');
-      continue;
-    } else if (option < 1 || option > 7) {
-      console.clearScreen();
-      mainMenu();
-      errorMessage('Choose an option between [1] — [7]');
-
-      option = int.tryParse(input(': ') ?? '');
-      continue;
-    }
-  }
-
-  switch (option) {
-    case 1:
-      return 'opt1';
-    case 2:
-      return 'opt2';
-    case 3:
-      return 'opt3';
-    case 4:
-      return 'opt4';
-    case 5:
-      return 'opt5';
-    case 6:
-      return 'opt6';
-    case 7:
-      return 'opt7';
-  }
-
-  return null;
-
 }
