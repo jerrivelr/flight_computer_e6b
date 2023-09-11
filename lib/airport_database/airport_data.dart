9b46e24b8437a9f3bbf8581756d7a26e085875af
Jerrivel Rodriguez <Jerrivelr@gmail.com>
2023-09-11 01:46:24 -0400

    Stores airport name and elevation

--- lib/airport_database/airport_data.dart
+++ lib/airport_database/airport_data.dart
@@ -1,26 +1,45 @@
 import 'dart:convert';
 import 'dart:io';
 
-final airportJson = File(r'C:\Users\jerri\IdeaProjects\flight_e6b\lib\airport_database\airports.json');
+import 'package:flight_e6b/simple_io.dart';
 
-int? airportElevation(String airportId) {
-  final content = airportJson.readAsStringSync();
-  final mapContent = jsonDecode(content);
+class AirportData {
+  static final _airportJson = File(r'C:\Users\jerri\IdeaProjects\flight_e6b\lib\airport_database\airports.json');
+  static final _content = _airportJson.readAsStringSync();
+  static final _mapContent = jsonDecode(_content);
 
-  try {
-    return mapContent[airportId]['elevation'] as int;
-  } on NoSuchMethodError {
-    return null;
+  AirportData(this._airportId);
+  final String _airportId;
+
+  String get airportId => _airportId;
+
+  factory AirportData.inputCheck() {
+    final idInput = input('Airport ID: ')?.toUpperCase();
+    final iataInput = RegExp(r'"icao": "(\w{4})",\n\s{8}"iata": "' + (idInput ?? '') + r'",');
+
+    if (iataInput.hasMatch(_content)) {
+      final icao = iataInput.firstMatch(_content)?.group(1);
+      return AirportData(icao ?? '');
+    }
+
+    return AirportData(idInput ?? '');
   }
-}
 
-String? airportName(String airportId) {
-  final content = airportJson.readAsStringSync();
-  final mapContent = jsonDecode(content);
+  Future<int?> airportElevation() async {
+    try {
+      return _mapContent[_airportId]['elevation'] as int;
+    } on NoSuchMethodError {
+      return null;
+    }
+  }
 
-  try {
-    return mapContent[airportId]['name'] as String;
-  } on NoSuchMethodError {
+  Future<String?> airportName() async {
+    final icao = _mapContent[_airportId]?['name'];
+
+    if (icao != null) {
+      return icao;
+    }
+
     return null;
   }
 }
