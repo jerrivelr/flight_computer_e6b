b49ad4ef725ae88cf22dd301790bb79c728004aa
Jerrivel Rodriguez <Jerrivelr@gmail.com>
2023-09-19 00:50:04 -0400

    formatError variable added

--- lib/communication_var.dart
+++ lib/communication_var.dart
@@ -5,9 +5,10 @@ final console = Console();
 // List of possible options while inside a certain screen.
 const optionList = ['opt1', 'opt2', 'opt3', 'opt4', 'opt5', 'opt6', 'opt7', 'menu', 'exit'];
 
+var error = ''; // Stores errors messages if any.
+String? selectedOption; // Stores values that are part of the optionList if userInput equals to one of the options
+Map<String, num> dataResult = {}; // This Map will contain the calculated data for reuse in other options.
 bool noInternet = false; // Checks when there is no internet.
 bool backOnline = false; // Checks when the internet comes back.
-String? selectedOption; // Stores values that are part of the optionList if userInput equals to one of the options
-var error = ''; // Stores errors messages if any.
-var screenCleared = false; // To check the screen has been clear
-Map<String, num> dataResult = {}; // This Map will contain the calculated data for reuse in other options.
+bool screenCleared = false; // To check the screen has been clear
+bool formatError = false; // Check if there is a format error with the downloaded json
