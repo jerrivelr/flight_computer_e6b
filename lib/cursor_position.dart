6cedd2805274724d416fb7ce0e0ac4a6771e57fa
Jerrivel Rodriguez <Jerrivelr@gmail.com>
2023-10-17 02:45:06 -0400

    New file contains functions for checking and changing cursor position

---
+++ lib/cursor_position.dart
@@ -0,0 +1,19 @@
+import 'package:dart_console/dart_console.dart';
+import 'package:flight_e6b/communication_var.dart' as comm;
+
+bool positionCheck(List<Coordinate> positions) {
+  if (comm.currentPosition > positions.length - 1) {
+    comm.console.clearScreen();
+    comm.currentCursorPos = Coordinate(comm.currentCursorPos!.row + 1, comm.console.cursorPosition!.col);
+    return true;
+  } else if (comm.currentPosition < 0) {
+    comm.currentPosition = 0;
+  }
+
+  return false;
+}
+
+void changePosition(List<Coordinate> positions) {
+  comm.console.cursorPosition = positions[comm.currentPosition];
+  comm.currentCursorPos = comm.console.cursorPosition;
+}
