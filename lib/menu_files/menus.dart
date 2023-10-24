949ee4a5e45514e063df009ba2343a570c22c3e6
Jerrivel Rodriguez <Jerrivelr@gmail.com>
2023-10-24 03:12:57 -0400

    new file contains all menus inside different options

---
+++ lib/menu_files/menus.dart
@@ -0,0 +1,58 @@
+import 'package:flight_e6b/menu_files/menu_logic.dart';
+import 'package:flight_e6b/menu_files/menu_builder.dart';
+
+final mainMenu = MenuBuilder(
+    title: 'FLIGHT COMPUTER (E6B)',
+    menuOptions: {
+      'Help/Config': OptionIdent.helpConfig,
+      'Cloud Base (ft)': OptionIdent.cloudBase,
+      'Pressure/Density Altitude (ft)': OptionIdent.pressDenAlt,
+      'Ground Speed (GS)': OptionIdent.groundSpeed,
+      'True Airspeed (TAS)': OptionIdent.trueAirspeed,
+      'Wind Component': OptionIdent.windComp,
+      'Heading/Wind Correction Angle (WCA)': OptionIdent.windCorrection,
+      'Fuel': OptionIdent.fuel,
+      'Exit': OptionIdent.exit
+    }
+); // Main Menu
+
+final helpConfigMenu = MenuBuilder(
+    title: 'HELP/CONFIG',
+    menuOptions: {
+      'Help': OptionIdent.help,
+      'Config': OptionIdent.config,
+      'Main Menu': OptionIdent.menu
+    }
+);
+
+final pressDenMenu = MenuBuilder(
+   title: 'PRESSURE/DENSITY ALTITUDE',
+   menuOptions: {
+     'Calculate Pressure/Density Altitude From...': null,
+     'Conditions at Airport': OptionIdent.airport,
+     'Manual Values': OptionIdent.manual,
+     'Main Menu': OptionIdent.menu
+   }
+);
+
+final groundSpeedMenu = MenuBuilder(
+   title: 'GROUND SPEED',
+   menuOptions: {
+     'Calculate...': null,
+     'Ground Speed (KT)': OptionIdent.calGroundSpeed,
+     'Duration (HR)': OptionIdent.groundDur,
+     'Distance (NM)': OptionIdent.groundDis,
+     'Main Menu': OptionIdent.menu
+   }
+);
+
+final fuelMenu = MenuBuilder(
+   title: 'FUEL',
+   menuOptions: {
+      'Calculate Fuel...': null,
+      'Volume (US Gal)': OptionIdent.fuelVol,
+      'Endurance (hr)': OptionIdent.fuelDur,
+      'Rate (US GPH)': OptionIdent.fuelRate,
+      'Main Menu': OptionIdent.menu
+   }
+);
