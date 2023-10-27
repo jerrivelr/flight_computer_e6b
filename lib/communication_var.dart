ed0e703eede74ef1103592499903c8197bb0161d
Jerrivel Rodriguez <Jerrivelr@gmail.com>
2023-10-27 19:29:20 -0400

    Type annotation removed

--- lib/communication_var.dart
+++ lib/communication_var.dart
@@ -5,15 +5,15 @@ final console = Console();
 
 var errorMessage = ''; // Stores errors messages if any.
 OptionIdent? selectedOption; // Stores values that are part of the optionList if userInput equals to one of the options.
-Map<String, num> dataResult = {}; // This Map will contain the calculated data for reuse in other options.
-Map<InputInfo?, String?> inputValues = {};
+var dataResult = <String, num>{}; // This Map will contain the calculated data for reuse in other options.
+var inputValues = <InputInfo?, String?>{};
 var currentCursorPos = console.cursorPosition;
 var currentPosition = 0; // Saves the current input selected
 
-bool noInternet = false; // Checks when there is no internet.
-bool screenCleared = false; // To check the screen has been clear.
-bool formatError = false; // Checks if there is a format error with the downloaded json.
-bool handShakeError = false; // Checks if there is a problem during the handShake phase.
-bool httpError = false; // Checks if there is a problem with http request.
-bool timeoutError = false; // Checks when the website takes too long to response.
+var noInternet = false; // Checks when there is no internet.
+var screenCleared = false; // To check the screen has been clear.
+var formatError = false; // Checks if there is a format error with the downloaded json.
+var handShakeError = false; // Checks if there is a problem during the handShake phase.
+var httpError = false; // Checks if there is a problem with http request.
+var timeoutError = false; // Checks when the website takes too long to response.
 
