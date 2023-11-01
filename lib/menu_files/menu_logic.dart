1f6ed41645f8447e46ae19b78c043ff5598098bf
Jerrivel Rodriguez <Jerrivelr@gmail.com>
2023-11-01 14:43:49 -0400

    enums moved to the enums.dart file

--- lib/menu_files/menu_logic.dart
+++ lib/menu_files/menu_logic.dart
@@ -1,54 +1,7 @@
 import 'package:flight_e6b/simple_io.dart';
 import 'package:dart_console/dart_console.dart';
+import 'package:flight_e6b/enums.dart';
 import 'package:flight_e6b/communication_var.dart' as comm;
-
-enum InputInfo {
-  temperature('Temperature: ', ' °C'),
-  dewpoint('Dewpoint: ', ' °C'),
-  indicatedAlt('Indicated Altitude: ', ' FT'),
-  baro('Baro: ', ' In Hg'),
-  distance('Distance: ', ' NM'),
-  time('Time: ', ' HR'),
-  calibratedAir('Calibrated Airspeed: ', ' KT'),
-  pressureAlt('Pressure Altitude: ', ' FT'),
-  windDirection('Wind Direction: ', '°'),
-  windSpeed('Wind Speed: ', ' KT'),
-  runway('Runway: ', '°'),
-  trueCourse('Course: ', '°'),
-  trueAirspeed('True Airspeed: ', ' KT'),
-  fuelVolume('Fuel Volume: ', ' GAL'),
-  groundSpeed('Ground Speed: ', ' KT'),
-  fuelRate('Fuel Rate: ', ' GAL/HR');
-
-  const InputInfo(this.title, this.unit);
-  final String title;
-  final String unit;
-}
-
-enum OptionIdent {
-  helpSetting,
-  help,
-  setting,
-  menu,
-  exit,
-  cloudBase,
-  pressDenAlt,
-  airport,
-  manual,
-  groundSpeed,
-  calGroundSpeed,
-  groundDur,
-  groundDis,
-  trueAirspeed,
-  windComp,
-  windCorrection,
-  fuel,
-  fuelVol,
-  fuelDur,
-  fuelRate,
-  yes,
-  no;
-}
 
 class MenuLogic {
   MenuLogic({
