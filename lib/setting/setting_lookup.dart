bf259b0ebf6209e274f24ff3a3aa1338cdd09888
Jerrivel Rodriguez <Jerrivelr@gmail.com>
2023-11-03 00:45:30 -0400

    function changed to show current file state

--- lib/setting/setting_lookup.dart
+++ lib/setting/setting_lookup.dart
@@ -2,21 +2,30 @@ import 'dart:io';
 
 import 'package:yaml/yaml.dart';
 
-final settingYaml = File(r'.\setting\setting.yaml').readAsStringSync();
-final settingDecoded = loadYaml(settingYaml) as YamlMap;
-
-final unitYaml = File(r'.\setting\units.yaml').readAsStringSync();
-final unitDecoded = loadYaml(unitYaml) as YamlMap;
+YamlMap? settingDecoded;
+YamlMap? unitDecoded;
 
 String? temperatureUnit() {
-  final tempMapBool = settingDecoded['selected_unit']['temperature'] as YamlMap;
-  final tempUnits = unitDecoded['temperature'] as YamlMap;
+  _updateFile();
 
-  for (final item in tempMapBool.keys) {
-    if (item.value == true) {
-      return tempUnits[item.key];
-    }
+  final tempMapBool = settingDecoded?['selected_unit']['temperature'] as YamlMap?;
+  final tempUnits = unitDecoded?['temperature'] as YamlMap?;
+
+  if (tempMapBool != null && tempUnits != null) {
+    for (final item in tempMapBool.entries) {
+        if (item.value == true) {
+          return tempUnits[item.key];
+        }
+      }
   }
 
   return null;
+}
+
+void _updateFile() {
+  final settingYaml = File(r'..\lib\setting\setting.yaml').readAsStringSync();
+  settingDecoded = loadYaml(settingYaml) as YamlMap;
+
+  final unitYaml = File(r'..\lib\setting\units.yaml').readAsStringSync();
+  unitDecoded = loadYaml(unitYaml) as YamlMap;
 }
