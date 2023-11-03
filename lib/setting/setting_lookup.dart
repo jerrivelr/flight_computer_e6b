e377a58c7b7a6cf4b3c6c3e8f11451f761cfd801
Jerrivel Rodriguez <Jerrivelr@gmail.com>
2023-11-03 02:37:59 -0400

    functions added to look for the selected unit

--- lib/setting/setting_lookup.dart
+++ lib/setting/setting_lookup.dart
@@ -39,6 +39,125 @@ String? altitudeUnit() {
   return null;
 }
 
+String? speedUnit() {
+  _updateFile();
+
+  final speedMapBool = settingDecoded?['selected_unit']['speed'] as YamlMap?;
+  final speedUnits = unitDecoded?['speed'] as YamlMap?;
+
+  if (speedMapBool != null && speedUnits != null) {
+    for (final item in speedMapBool.entries) {
+      if (item.value == true) {
+        return speedUnits[item.key];
+      }
+    }
+  }
+
+  return null;
+}
+
+String? distanceUnit() {
+  _updateFile();
+
+  final disMapBool = settingDecoded?['selected_unit']['distance'] as YamlMap?;
+  final disUnits = unitDecoded?['distance'] as YamlMap?;
+
+  if (disMapBool != null && disUnits != null) {
+    for (final item in disMapBool.entries) {
+      if (item.value == true) {
+        return disUnits[item.key];
+      }
+    }
+  }
+
+  return null;
+}
+
+String? timeUnit() {
+  _updateFile();
+
+  final timeMapBool = settingDecoded?['selected_unit']['time'] as YamlMap?;
+  final timeUnits = unitDecoded?['time'] as YamlMap?;
+
+  if (timeMapBool != null && timeUnits != null) {
+    for (final item in timeMapBool.entries) {
+      if (item.value == true) {
+        return timeUnits[item.key];
+      }
+    }
+  }
+
+  return null;
+}
+
+String? pressUnit() {
+  _updateFile();
+
+  final pressMapBool = settingDecoded?['selected_unit']['pressure'] as YamlMap?;
+  final pressUnits = unitDecoded?['pressure'] as YamlMap?;
+
+  if (pressMapBool != null && pressUnits != null) {
+    for (final item in pressMapBool.entries) {
+      if (item.value == true) {
+        return pressUnits[item.key];
+      }
+    }
+  }
+
+  return null;
+}
+
+String? weightUnit() {
+  _updateFile();
+
+  final weightMapBool = settingDecoded?['selected_unit']['weight'] as YamlMap?;
+  final weightUnits = unitDecoded?['weight'] as YamlMap?;
+
+  if (weightMapBool != null && weightUnits != null) {
+    for (final item in weightMapBool.entries) {
+      if (item.value == true) {
+        return weightUnits[item.key];
+      }
+    }
+  }
+
+  return null;
+}
+
+String? fuelRateUnit() {
+  _updateFile();
+
+  final rateMapBool = settingDecoded?['selected_unit']['fuel_rate'] as YamlMap?;
+  final rateUnits = unitDecoded?['fuel_rate'] as YamlMap?;
+
+  if (rateMapBool != null && rateUnits != null) {
+    for (final item in rateMapBool.entries) {
+      if (item.value == true) {
+        return rateUnits[item.key];
+      }
+    }
+  }
+
+  return null;
+}
+
+String? fuelUnit() {
+  _updateFile();
+
+  final fuelMapBool = settingDecoded?['selected_unit']['fuel_volume'] as YamlMap?;
+  final fuelUnits = unitDecoded?['fuel_volume'] as YamlMap?;
+
+  if (fuelMapBool != null && fuelUnits != null) {
+    for (final item in fuelMapBool.entries) {
+      if (item.value == true) {
+        return fuelUnits[item.key];
+      }
+    }
+  }
+
+  return null;
+}
+
 void _updateFile() {
   final settingYaml = File(r'..\lib\setting\setting.yaml').readAsStringSync();
   settingDecoded = loadYaml(settingYaml) as YamlMap;
