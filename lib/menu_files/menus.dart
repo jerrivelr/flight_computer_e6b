090533b75a252e09639f1ab302905e649f591fc1
Jerrivel Rodriguez <Jerrivelr@gmail.com>
2023-11-03 02:39:03 -0400

    menu show selected unit in parentheses

--- lib/menu_files/menus.dart
+++ lib/menu_files/menus.dart
@@ -1,14 +1,16 @@
 import 'package:flight_e6b/menu_files/menu_builder.dart';
 import 'package:flight_e6b/enums.dart';
 
+import '../setting/setting_lookup.dart';
+
 final mainMenu = MenuBuilder(
     title: 'FLIGHT COMPUTER (E6B)',
     menuOptions: {
       'Help/Settings': OptionIdent.helpSetting,
-      'Cloud Base (ft)': OptionIdent.cloudBase,
-      'Pressure/Density Altitude (ft)': OptionIdent.pressDenAlt,
-      'Ground Speed (GS)': OptionIdent.groundSpeed,
-      'True Airspeed (TAS)': OptionIdent.trueAirspeed,
+      'Cloud Base (${altitudeUnit()?.trim()})': OptionIdent.cloudBase,
+      'Pressure/Density Altitude (${altitudeUnit()?.trim()})': OptionIdent.pressDenAlt,
+      'Ground Speed (${speedUnit()?.trim()})': OptionIdent.groundSpeed,
+      'True Airspeed (${speedUnit()?.trim()})': OptionIdent.trueAirspeed,
       'Wind Component': OptionIdent.windComp,
       'Heading/Wind Correction Angle (WCA)': OptionIdent.windCorrection,
       'Fuel': OptionIdent.fuel,
@@ -39,7 +41,7 @@ final groundSpeedMenu = MenuBuilder(
    title: 'GROUND SPEED',
    menuOptions: {
      'Calculate...': null,
-     'Ground Speed (KT)': OptionIdent.calGroundSpeed,
+     'Ground Speed (${speedUnit()?.trim()})': OptionIdent.calGroundSpeed,
      'Duration (HR)': OptionIdent.groundDur,
      'Distance (NM)': OptionIdent.groundDis,
      'Main Menu': OptionIdent.menu
@@ -51,7 +53,7 @@ final fuelMenu = MenuBuilder(
    menuOptions: {
       'Calculate Fuel...': null,
       'Volume (US Gal)': OptionIdent.fuelVol,
-      'Endurance (hr)': OptionIdent.fuelDur,
+      'Endurance (${timeUnit()?.trim()})': OptionIdent.fuelDur,
       'Rate (US GPH)': OptionIdent.fuelRate,
       'Main Menu': OptionIdent.menu
    }
