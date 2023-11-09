ce8033440c6129fdb7e361f548a3632eeb5a645a
Jerrivel Rodriguez <Jerrivelr@gmail.com>
2023-11-09 18:10:23 -0500

    Null assertion deleted

--- lib/cursor_position.dart
+++ lib/cursor_position.dart
@@ -4,7 +4,7 @@ import 'package:flight_e6b/communication_var.dart' as comm;
 bool positionCheck(List<Coordinate> positions) {
   if (comm.currentPosition > positions.length - 1) {
     comm.console.clearScreen();
-    comm.currentCursorPos = Coordinate(comm.currentCursorPos!.row + 1, comm.console.cursorPosition!.col);
+    comm.currentCursorPos = Coordinate((comm.currentCursorPos?.row ?? 0) + 1, comm.console.cursorPosition?.col ?? 0);
     return true;
   } else if (comm.currentPosition < 0) {
     comm.currentPosition = 0;
@@ -15,5 +15,5 @@ bool positionCheck(List<Coordinate> positions) {
 
 void changePosition(List<Coordinate> positions) {
   comm.console.cursorPosition = positions[comm.currentPosition];
-  comm.currentCursorPos = comm.console.cursorPosition;
+  comm.currentCursorPos = positions[comm.currentPosition];
 }
