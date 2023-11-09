7ceeb4a35bdff477b8424229988c47b5e305db5d
Jerrivel Rodriguez <Jerrivelr@gmail.com>
2023-11-09 18:08:28 -0500

    variables and updateYamlFile tracks boolean values of each key

--- lib/communication_var.dart
+++ lib/communication_var.dart
@@ -1,5 +1,6 @@
 import 'package:dart_console/dart_console.dart';
 import 'package:flight_e6b/enums.dart';
+import 'package:yaml/yaml.dart';
 
 final console = Console();
 
@@ -16,4 +17,62 @@ var httpError = false; // Checks if there is a problem with http request.
 var timeoutError = false; // Checks when the website takes too long to response.
 
 ControlCharacter? keyPressed; // to catch which was pressed
+
+var celsiusTrue = false;
+var fahrenheitTrue = false;
+var feetTrue = false;
+var metersTrue = false;
+var inchesMercuryTrue = false;
+var millibarsTrue = false;
+var knotsTrue = false;
+var kilometerHoursTrue = false;
+var milesHoursTrue = false;
+var disFeetTrue = false;
+var disKilometerTrue = false;
+var disMetersTrue = false;
+var minuteTrue = false;
+var secondTrue = false;
+var kilogramsTrue = false;
+var fuelPoundTrue = false;
+var fuelLiterTrue = false;
+var jetA = false;
+var jetB = false;
+
+YamlMap? settingDecoded;
+YamlMap? unitDecoded;
+
+void updateYamlFile() {
+  final settingYaml = File(r'..\lib\setting\setting.yaml').readAsStringSync();
+  final unitYaml = File(r'..\lib\setting\units.yaml').readAsStringSync();
+  settingDecoded = loadYaml(settingYaml);
+  unitDecoded = loadYaml(unitYaml);
+
+  celsiusTrue = settingDecoded?['selected_unit']['Temperature']['celsius'] as bool;
+  fahrenheitTrue = settingDecoded?['selected_unit']['Temperature']['fahrenheit'] as bool;
+
+  inchesMercuryTrue = settingDecoded?['selected_unit']['Pressure']['inches_of_mercury'] as bool;
+  millibarsTrue = settingDecoded?['selected_unit']['Pressure']['millibars'] as bool;
+
+  feetTrue = settingDecoded?['selected_unit']['Altitude']['feet'] as bool;
+  metersTrue = settingDecoded?['selected_unit']['Altitude']['meters'] as bool;
+
+  knotsTrue = settingDecoded?['selected_unit']['Speed']['knots'] as bool;
+  kilometerHoursTrue = settingDecoded?['selected_unit']['Speed']['kilometer_hours'] as bool;
+  milesHoursTrue = settingDecoded?['selected_unit']['Speed']['milesHour'] as bool;
+
+  disFeetTrue = settingDecoded?['selected_unit']['Distance']['dis_feet'] as bool;
+  disKilometerTrue = settingDecoded?['selected_unit']['Distance']['kilometers'] as bool;
+  disMetersTrue = settingDecoded?['selected_unit']['Distance']['dis_meters'] as bool;
+
+  minuteTrue = settingDecoded?['selected_unit']['Time']['minute'] as bool;
+  secondTrue = settingDecoded?['selected_unit']['Time']['seconds'] as bool;
+
+  kilogramsTrue = settingDecoded?['selected_unit']['Fuel Weight']['kilograms'] as bool;
+
+  fuelPoundTrue = settingDecoded?['selected_unit']['Fuel Volume']['fuel_pound'] as bool;
+  fuelLiterTrue = settingDecoded?['selected_unit']['Fuel Volume']['fuel_liter'] as bool;
+
+  jetA = settingDecoded?['selected_unit']['Fuel Type']['jet_a'] as bool;
+  jetB = settingDecoded?['selected_unit']['Fuel Type']['jet_b'] as bool;
+}
 
